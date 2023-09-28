7f1cb52fc78ea2ac64f543d2b09265839218e904
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-09-29 00:30:28 +0700

    fix: image 404

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,9 @@ export default () => {
     },
     server: {
       port: 3000,
-      proxy: 'https://pixinvent.com/',
+      proxy: 'https://ismilelab-telu.com/',
       cors: {
-        origin: ['https://pixinvent.com/', 'http://localhost:3000'],
+        origin: ['https://ismilelab-telu.com/', 'http://localhost:3000'],
         methods: ['GET', 'PATCH', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
       }
