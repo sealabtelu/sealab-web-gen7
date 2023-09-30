b2ffda811215b96ce7fcf90be47e23722aff31c1
HimejiSoul <perseushilmy@gmail.com>
2023-10-01 01:13:37 +0700

    feat(PreTest): add instruction

--- src/views/pages/student/PreTest/PreTest.js
+++ src/views/pages/student/PreTest/PreTest.js
@@ -1,78 +1,100 @@
-import React, { useState,useEffect } from 'react'; // Import React
+import React, { useState, useEffect } from "react"; // Import React
 import { Card, CardHeader, CardTitle, Button } from "reactstrap";
-import PreTestOverlay from "./PreTestOverlay"
-import { useDispatch, useSelector } from 'react-redux'
-import { getModules } from '@store/api/module';
-
+import PreTestOverlay from "./PreTestOverlay";
+import { useDispatch, useSelector } from "react-redux";
+import { getModules } from "@store/api/module";
 
 export default function PreTest() {
+	const [isOpenClicked, setIsOpenClicked] = useState(null);
+	const handleOpenClick = (moduleId) => {
+		setIsOpenClicked(moduleId);
+	};
 
-    const [isOpenClicked, setIsOpenClicked] = useState(null);
+	const dispatch = useDispatch();
+	const module = useSelector((state) => state.module);
 
-    const handleOpenClick = (moduleId) => {
-        setIsOpenClicked(moduleId);
-    };
+	useEffect(() => {
+		dispatch(getModules());
+	}, []);
 
-    const dispatch = useDispatch()
-    const module = useSelector(state => state.module)
+	return (
+		<div>
+			<Card className="card-overlay-jurnal">
+				{/* INFO */}
+				<h1>Tes Awal</h1>
+				<ol type="1">
+					<li>
+						Setiap mahasiswa diharapkan hadir <b> tepat waktu </b> sesuai jadwal
+						tes awal.
+					</li>
+					<li>
+						Tes Awal diberikan sebelum pelaksanaan praktikum dimulai dan
+						dilaksanakan maksimal selama <b>20 menit.</b>
+					</li>
+					<li>
+						Jumlah soal yang dikerjakan praktikan sebanyak 10 butir soal dengan
+						tipe soal adalah Multiple choice (Pilihan Ganda) soal acak
+					</li>
+					<li>
+						Apabila praktikan datang pada saat tes awal sedang berlangsung, maka
+						praktikan diperbolehkan mengikuti tes awal{" "}
+						<b>tanpa ada tambahan waktu.</b>
+					</li>
+					<li>
+						Mahasiswa diharapkan mematuhi semua instruksi yang diberikan oleh
+						asisten praktikum
+					</li>
+					<li>
+						<b>Dilarang</b> berbicara atau berkomunikasi dengan mahasiswa lain
+						selama tes berlangsung.
+					</li>
+					<li>
+						Tes awal bersifat <b>close all</b>, praktikan <b>dilarang</b>{" "}
+						menggunakan bahan referensi atau sumber lain selama mengerjakan
+						kuis.
+					</li>
+					<li>
+						Praktikan diwajibkan <b>menjaga etika</b> yang baik selama tes
+						berlangsung.
+					</li>
+					<li>
+						<b>Dilarang</b> mencoba melakukan tindakan curang, menyalin jawaban
+						mahasiswa lain, atau menggunakan perangkat lain selain komputer yang
+						telah disediakan.
+					</li>
+					<li>
+						Apabila praktikan <b>terbukti</b> melakukan tindak kecurangan
+						pengerjaan tes awal dalam bentuk apapun, maka <b>nilai TA = 0</b>
+					</li>
+				</ol>
+			</Card>
+			{module.modules.map((item, index) => (
+				<div key={item.id}>
+					{isOpenClicked === item.id && (
+						<PreTestOverlay
+							moduleTitle={item.name}
+							moduleNumber={index + 1}
+							item={item}
+						/>
+					)}
 
-    useEffect(() => {
-        dispatch(getModules())
-    }, [])
-
-    return (
-        <div>
-            <Card className="card-overlay-jurnal">
-            {/* INFO */}
-            <h1>HARAP DIBACA</h1>
-                <ol type="1">
-                    <li>
-                        Tugas Pendahuluan dikerjakan sesuai dengan NIM terakhir Praktikan.<br />
-                        <b>Contoh:</b> 1103211233 [mengerjakan soal NIM Ganjil]<br />
-                        <b>Contoh:</b> 1103211234 [mengerjakan soal NIM Genap]
-                    </li>
-                    <li>
-                        Tugas Pendahuluan dikerjakan menggunakan Template Tugas Pendahuluan di word dan dikumpulkan dalam format PDF
-                    </li>
-                    <li>
-                        Jika terdapat soal yang memerlukan jawaban untuk ditulis manual maka jawaban dapat di foto/scan dan kemudian di masukan ke dalam File Tugas Pendahuluan.
-                    </li>
-                    <li>
-                        Jawaban Tugas Pendahuluan diketik secara berurutan. Soal kemudian Jawaban.
-                    </li>
-                    <li>
-                        Format penamaan file Tugas Pendahuluan sebagai berikut:<br />
-                        <b>TP_NAMA_NIM_MODUL_HARI_SHIFT&shy;_KELOMPOK</b><br />
-                        <b>Contoh: TP_MUHAMMAD HILMY AZIZ_1103190001_MODUL 1_RABU_SHIFT 2_13</b>
-                    </li>
-                    <li>
-                        Tugas Pendahuluan dikumpulkan melalui website i-Smile dengan batas pengumpulan <b>Hari Sabtu 23:59 WIB.</b>
-                    </li>
-                    <li>
-                        Seluruh informasi kebutuhan untuk Praktikum Kecerdasan Buatan dapat diakses melalui Website i-Smile.
-                    </li>
-                </ol>
-        </Card>
-        {module.modules.map((item, index) => (
-            <div key={item.id}>
-            {isOpenClicked === item.id && (
-                <PreTestOverlay
-                moduleTitle={item.name}
-                moduleNumber={index + 1}
-                item={item}
-                />
-            )}
-
-            {isOpenClicked !== item.id && (
-                <Card className='card-student'>
-                    <CardHeader>
-                        <CardTitle>Modul {index + 1} - {item.name}</CardTitle>
-                        <Button color="relief-primary" onClick={() => handleOpenClick(item.id)}>Open</Button>
-                    </CardHeader>
-                </Card>
-            )}
-        </div>
-        ))}
-        </div>
-    );
+					{isOpenClicked !== item.id && (
+						<Card className="card-student">
+							<CardHeader>
+								<CardTitle>
+									Modul {index + 1} - {item.name}
+								</CardTitle>
+								<Button
+									color="relief-primary"
+									onClick={() => handleOpenClick(item.id)}
+								>
+									Open
+								</Button>
+							</CardHeader>
+						</Card>
+					)}
+				</div>
+			))}
+		</div>
+	);
 }
