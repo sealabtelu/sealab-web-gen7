1413ac9430cacf73da2450350086ac9c2e19756f
HimejiSoul <perseushilmy@gmail.com>
2023-10-01 01:13:25 +0700

    feat(Journal): add instruction

--- src/views/pages/student/Journal/Journal.js
+++ src/views/pages/student/Journal/Journal.js
@@ -1,78 +1,66 @@
-import React, { useState,useEffect } from 'react'; // Import React
+import React, { useState, useEffect } from "react"; // Import React
 import { Card, CardHeader, CardTitle, Button } from "reactstrap";
-import OverlayJurnal from "./JournalOverlay"
-import { useDispatch, useSelector } from 'react-redux'
-import { getModules } from '@store/api/module';
-
+import OverlayJurnal from "./JournalOverlay";
+import { useDispatch, useSelector } from "react-redux";
+import { getModules } from "@store/api/module";
 
 export default function Journal() {
+	const [isOpenClicked, setIsOpenClicked] = useState(null);
 
-    const [isOpenClicked, setIsOpenClicked] = useState(null);
+	const handleOpenClick = (moduleId) => {
+		setIsOpenClicked(moduleId);
+	};
 
-    const handleOpenClick = (moduleId) => {
-        setIsOpenClicked(moduleId);
-    };
+	const dispatch = useDispatch();
+	const module = useSelector((state) => state.module);
 
-    const dispatch = useDispatch()
-    const module = useSelector(state => state.module)
+	useEffect(() => {
+		dispatch(getModules());
+	}, []);
 
-    useEffect(() => {
-        dispatch(getModules())
-    }, [])
+	return (
+		<div>
+			<Card className="card-overlay-jurnal">
+				{/* INFO */}
+				<h1>Jurnal</h1>
+				<ol type="1">
+					<li>
+						Jurnal dikerjakan ketika praktikum berlangsung dan dikerjakan sesuai
+						dengan <b>arahan asisten.</b>
+					</li>
+					<li>
+						Pengumpulan Jurnal sesuai dengan <b>arahan dari asisten</b>{" "}
+						Laboratorium I-Smile.
+					</li>
+				</ol>
+			</Card>
+			{module.modules.map((item, index) => (
+				<div key={item.id}>
+					{isOpenClicked === item.id && (
+						<OverlayJurnal
+							moduleTitle={item.name}
+							moduleNumber={index + 1}
+							// linkSoal={module.links.soal}
+						/>
+					)}
 
-    return (
-        <div>
-            <Card className="card-overlay-jurnal">
-            {/* INFO */}
-            <h1>HARAP DIBACA</h1>
-                <ol type="1">
-                    <li>
-                        Tugas Pendahuluan dikerjakan sesuai dengan NIM terakhir Praktikan.<br />
-                        <b>Contoh:</b> 1103211233 [mengerjakan soal NIM Ganjil]<br />
-                        <b>Contoh:</b> 1103211234 [mengerjakan soal NIM Genap]
-                    </li>
-                    <li>
-                        Tugas Pendahuluan dikerjakan menggunakan Template Tugas Pendahuluan di word dan dikumpulkan dalam format PDF
-                    </li>
-                    <li>
-                        Jika terdapat soal yang memerlukan jawaban untuk ditulis manual maka jawaban dapat di foto/scan dan kemudian di masukan ke dalam File Tugas Pendahuluan.
-                    </li>
-                    <li>
-                        Jawaban Tugas Pendahuluan diketik secara berurutan. Soal kemudian Jawaban.
-                    </li>
-                    <li>
-                        Format penamaan file Tugas Pendahuluan sebagai berikut:<br />
-                        <b>TP_NAMA_NIM_MODUL_HARI_SHIFT&shy;_KELOMPOK</b><br />
-                        <b>Contoh: TP_MUHAMMAD HILMY AZIZ_1103190001_MODUL 1_RABU_SHIFT 2_13</b>
-                    </li>
-                    <li>
-                        Tugas Pendahuluan dikumpulkan melalui website i-Smile dengan batas pengumpulan <b>Hari Sabtu 23:59 WIB.</b>
-                    </li>
-                    <li>
-                        Seluruh informasi kebutuhan untuk Praktikum Kecerdasan Buatan dapat diakses melalui Website i-Smile.
-                    </li>
-                </ol>
-        </Card>
-        {module.modules.map((item, index) => (
-            <div key={item.id}>
-            {isOpenClicked === item.id && (
-                <OverlayJurnal
-                moduleTitle={item.name}
-                moduleNumber={index + 1}
-                // linkSoal={module.links.soal}
-                />
-            )}
-
-            {isOpenClicked !== item.id && (
-                <Card className='card-student'>
-                    <CardHeader>
-                        <CardTitle>Modul {index + 1} - {item.name}</CardTitle>
-                        <Button color="relief-primary" onClick={() => handleOpenClick(item.id)}>Open</Button>
-                    </CardHeader>
-                </Card>
-            )}
-        </div>
-        ))}
-        </div>
-    );
+					{isOpenClicked !== item.id && (
+						<Card className="card-student">
+							<CardHeader>
+								<CardTitle>
+									Modul {index + 1} - {item.name}
+								</CardTitle>
+								<Button
+									color="relief-primary"
+									onClick={() => handleOpenClick(item.id)}
+								>
+									Open
+								</Button>
+							</CardHeader>
+						</Card>
+					)}
+				</div>
+			))}
+		</div>
+	);
 }
--- src/views/pages/student/Journal/JournalOverlay.js
+++ src/views/pages/student/Journal/JournalOverlay.js
@@ -1,124 +1,171 @@
 /* eslint-disable react/prop-types */
-import React,{useState,Fragment} from "react";
-import { Card, Button, Row, Col, Container,Collapse,CardBody,ListGroup, ListGroupItem } from "reactstrap";
-import { Link } from 'react-router-dom'
-import { Upload } from 'react-feather';
+import React, { useState, Fragment } from "react";
+import {
+	Card,
+	Button,
+	Row,
+	Col,
+	Container,
+	Collapse,
+	CardBody,
+	ListGroup,
+	ListGroupItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Upload } from "react-feather";
 
 // ** Third Party Imports File Uploader
-import { useDropzone } from 'react-dropzone'
-import { FileText, X, DownloadCloud } from 'react-feather'
+import { useDropzone } from "react-dropzone";
+import { FileText, X, DownloadCloud } from "react-feather";
 
-export default function PreTestOverlay({ moduleTitle, moduleNumber}) {
-    
-    //collase
-    const [isOpen, setIsOpen] = useState(false)
+export default function PreTestOverlay({ moduleTitle, moduleNumber }) {
+	//collase
+	const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen)
+	const toggle = () => setIsOpen(!isOpen);
 
-    //Input File code
-    const [files, setFiles] = useState([])
+	//Input File code
+	const [files, setFiles] = useState([]);
 
-    const { getRootProps, getInputProps } = useDropzone({
-    multiple: false,
-    onDrop: acceptedFiles => {
-        setFiles([...files, ...acceptedFiles.map(file => Object.assign(file))])
-    }
-    })
+	const { getRootProps, getInputProps } = useDropzone({
+		multiple: false,
+		onDrop: (acceptedFiles) => {
+			setFiles([...files, ...acceptedFiles.map((file) => Object.assign(file))]);
+		},
+	});
 
-    const renderFilePreview = file => {
-    if (file.type.startsWith('image')) {
-        return <img className='rounded' alt={file.name} src={URL.createObjectURL(file)} height='28' width='28' />
-    } else {
-        return <FileText size='28' />
-    }
-    }
+	const renderFilePreview = (file) => {
+		if (file.type.startsWith("image")) {
+			return (
+				<img
+					className="rounded"
+					alt={file.name}
+					src={URL.createObjectURL(file)}
+					height="28"
+					width="28"
+				/>
+			);
+		} else {
+			return <FileText size="28" />;
+		}
+	};
 
-    const handleRemoveFile = file => {
-    const uploadedFiles = files
-    const filtered = uploadedFiles.filter(i => i.name !== file.name)
-    setFiles([...filtered])
-    }
+	const handleRemoveFile = (file) => {
+		const uploadedFiles = files;
+		const filtered = uploadedFiles.filter((i) => i.name !== file.name);
+		setFiles([...filtered]);
+	};
 
-    const renderFileSize = size => {
-    if (Math.round(size / 100) / 10 > 1000) {
-        return `${(Math.round(size / 100) / 10000).toFixed(1)} mb`
-    } else {
-        return `${(Math.round(size / 100) / 10).toFixed(1)} kb`
-    }
-    }
+	const renderFileSize = (size) => {
+		if (Math.round(size / 100) / 10 > 1000) {
+			return `${(Math.round(size / 100) / 10000).toFixed(1)} mb`;
+		} else {
+			return `${(Math.round(size / 100) / 10).toFixed(1)} kb`;
+		}
+	};
 
-    const fileList = files.map((file, index) => (
-    <ListGroupItem key={`${file.name}-${index}`} className='d-flex align-items-center justify-content-between'>
-        <div className='file-details d-flex align-items-center'>
-        <div className='file-preview me-1'>{renderFilePreview(file)}</div>
-        <div>
-            <p className='file-name mb-0'>{file.name}</p>
-            <p className='file-size mb-0'>{renderFileSize(file.size)}</p>
-        </div>
-        </div>
-        <Button color='danger' outline size='sm' className='btn-icon' onClick={() => handleRemoveFile(file)}>
-        <X size={14} />
-        </Button>
-    </ListGroupItem>
-    ))
+	const fileList = files.map((file, index) => (
+		<ListGroupItem
+			key={`${file.name}-${index}`}
+			className="d-flex align-items-center justify-content-between"
+		>
+			<div className="file-details d-flex align-items-center">
+				<div className="file-preview me-1">{renderFilePreview(file)}</div>
+				<div>
+					<p className="file-name mb-0">{file.name}</p>
+					<p className="file-size mb-0">{renderFileSize(file.size)}</p>
+				</div>
+			</div>
+			<Button
+				color="danger"
+				outline
+				size="sm"
+				className="btn-icon"
+				onClick={() => handleRemoveFile(file)}
+			>
+				<X size={14} />
+			</Button>
+		</ListGroupItem>
+	));
 
-    const handleRemoveAllFiles = () => {
-    setFiles([])
-    }
+	const handleRemoveAllFiles = () => {
+		setFiles([]);
+	};
 
-    return (
-        <Card className="card-overlay-jurnal">
-            {/* SUBMIT */}
-            <Container>
-                <Row>
-                    <Col>
-                        {/* <h2>Tugas Pendahuluan</h2> */}
-                        <h3 className="title-overlay"><b>MODUL {moduleNumber}: {moduleTitle}</b></h3>
-                        <p><b>Due Date: </b> &nbsp; </p>
-                        <p><b>Time Remaining:</b> &nbsp; 1 Hour 23 Min</p>
-                        <p><b>Time Submitted:</b> &nbsp; </p>
-                    </Col>
-                    <Col xs='12' sm='6'>
-                        <div className="button-container">
-                            <Button tag={Link} to='' color="relief-primary" >Soal Jurnal</Button>
-                            <Button color='flat-dark' onClick={toggle}>
-                                Submit File <Upload size={12} style={{ marginLeft: '5px', color:'black' }} />
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
-                <Collapse isOpen={isOpen}>
-                                <Card>
-                                    <CardBody>
-                                        <div {...getRootProps({ className: 'dropzone' })}>
-                                        <input {...getInputProps()} />
-                                        <div className='d-flex align-items-center justify-content-center flex-column'>
-                                            <DownloadCloud size={64} />
-                                            <h5>Drop Files here or click to upload</h5>
-                                            <p className='text-secondary'>
-                                            Drop files here or click{' '}
-                                            <a href='/' onClick={e => e.preventDefault()}>
-                                                browse
-                                            </a>{' '}
-                                            thorough your machine
-                                            </p>
-                                        </div>
-                                        </div>
-                                        {files.length ? (
-                                        <Fragment>
-                                            <ListGroup className='my-2'>{fileList}</ListGroup>
-                                            <div className='d-flex justify-content-end'>
-                                            <Button className='me-1' color='danger' outline onClick={handleRemoveAllFiles}>
-                                                Remove All
-                                            </Button>
-                                            <Button color='relief-primary'>Upload Files</Button>
-                                            </div>
-                                        </Fragment>
-                                        ) : null}
-                                    </CardBody>
-                                </Card>
-                            </Collapse>
-            </Container>
-        </Card>
-    )
+	return (
+		<Card className="card-overlay-jurnal">
+			{/* SUBMIT */}
+			<Container>
+				<Row>
+					<Col>
+						{/* <h2>Tugas Pendahuluan</h2> */}
+						<h3 className="title-overlay">
+							<b>
+								MODUL {moduleNumber}: {moduleTitle}
+							</b>
+						</h3>
+						<p>
+							<b>Due Date: </b> &nbsp;{" "}
+						</p>
+						<p>
+							<b>Time Remaining:</b> &nbsp; 1 Hour 23 Min
+						</p>
+						<p>
+							<b>Time Submitted:</b> &nbsp;{" "}
+						</p>
+					</Col>
+					<Col xs="12" sm="6">
+						<div className="button-container">
+							<Button tag={Link} to="" color="relief-primary">
+								Soal Jurnal
+							</Button>
+							<Button color="flat-dark" onClick={toggle}>
+								Submit File{" "}
+								<Upload
+									size={12}
+									style={{ marginLeft: "5px", color: "black" }}
+								/>
+							</Button>
+						</div>
+					</Col>
+				</Row>
+				<Collapse isOpen={isOpen}>
+					<Card>
+						<CardBody>
+							<div {...getRootProps({ className: "dropzone" })}>
+								<input {...getInputProps()} />
+								<div className="d-flex align-items-center justify-content-center flex-column">
+									<DownloadCloud size={64} />
+									<h5>Drop Files here or click to upload</h5>
+									<p className="text-secondary">
+										Drop files here or click{" "}
+										<a href="/" onClick={(e) => e.preventDefault()}>
+											browse
+										</a>{" "}
+										thorough your machine
+									</p>
+								</div>
+							</div>
+							{files.length ? (
+								<Fragment>
+									<ListGroup className="my-2">{fileList}</ListGroup>
+									<div className="d-flex justify-content-end">
+										<Button
+											className="me-1"
+											color="danger"
+											outline
+											onClick={handleRemoveAllFiles}
+										>
+											Remove All
+										</Button>
+										<Button color="relief-primary">Upload Files</Button>
+									</div>
+								</Fragment>
+							) : null}
+						</CardBody>
+					</Card>
+				</Collapse>
+			</Container>
+		</Card>
+	);
 }
