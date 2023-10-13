af3e068a1f982e42c6d00eb393fbf05dc1f96948
HimejiSoul <perseushilmy@gmail.com>
2023-10-13 20:35:02 +0700

    feat(profile): add submit function

--- src/views/pages/student/Profile/AccountTabContent.js
+++ src/views/pages/student/Profile/AccountTabContent.js
@@ -1,74 +1,78 @@
 // ** React Imports
-import { Fragment } from 'react'
+import { Fragment } from "react";
 
 // ** Third Party Components
-import Select from 'react-select'
-import Cleave from 'cleave.js/react'
-import { useForm, Controller } from 'react-hook-form'
-import 'cleave.js/dist/addons/cleave-phone.us'
+import Select from "react-select";
+import toast from "react-hot-toast";
+import { useForm, Controller } from "react-hook-form";
+import { getUserData } from "@utils";
+import { useDispatch, useSelector } from "react-redux";
+import { editStudent } from "@store/api/user";
 
 // ** Reactstrap Imports
-import { Row, Col, Form, Card, Input, Label, Button, CardBody, CardTitle, CardHeader, FormFeedback } from 'reactstrap'
+import {
+	Row,
+	Col,
+	Form,
+	Card,
+	Input,
+	Label,
+	Button,
+	CardBody,
+	CardTitle,
+	CardHeader,
+	FormFeedback,
+	Spinner,
+} from "reactstrap";
 
 // ** Utils
-import { selectThemeColors } from '@utils'
+import { selectThemeColors } from "@utils";
 
 // ** Demo Components
-import SecurityTabContent from './SecurityTabContent'
-
-const dayOptions = [
-  { value: 'senin', label: 'Senin' },
-  { value: 'selasa', label: 'Selasa' },
-  { value: 'rabu', label: 'Rabu' },
-  { value: 'kamis', label: 'Kamis' },
-  { value: 'jumat', label: 'Jumat' },
-  { value: 'sabtu', label: 'Sabtu' }
-]
-
-const shiftOptions = [
-  { value: '1', label: '1' },
-  { value: '2', label: '2' },
-  { value: '3', label: '3' },
-  { value: '4', label: '4' }
-]
+import SecurityTabContent from "./SecurityTabContent";
 
 // const AccountTabs = ({ data }) => {
 const AccountTabs = () => {
-  // ** Hooks
-  const defaultValues = {
-    lastName: '',
-    // firstName: data.fullName.split(' ')[0]
-    firstName: ''
-  }
-  const {
-    control,
-    setError,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({ defaultValues })
+	const dispatch = useDispatch();
+	// ** Hooks
+	const { dayOptions, shiftOptions } = useSelector((state) => state.seelabs);
+	const { isLoading } = useSelector((state) => state.user);
+	const defaultValues = {
+		...getUserData(),
+		id: getUserData().idStudent,
+		day: dayOptions[getUserData().day - 1],
+		shift: shiftOptions[getUserData().shift - 1],
+	};
+	const {
+		control,
+		setError,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ defaultValues });
 
-  const onSubmit = data => {
-    if (Object.values(data).every(field => field.length > 0)) {
-      return null
-    } else {
-      for (const key in data) {
-        if (data[key].length === 0) {
-          setError(key, {
-            type: 'manual'
-          })
-        }
-      }
-    }
-  }
+	const onSubmit = (data) => {
+		dispatch(
+			editStudent({ ...data, day: data.day.value, shift: data.shift.value })
+		)
+			.then(({ payload: { status } }) => {
+				if (status === 200) {
+					toast.success("Update Successfully!");
+				} else {
+					// console.log(status);
+					toast.error("Update failed!");
+				}
+			})
+			.catch(() => toast.error("Unexpected Error!"));
+	};
 
-  return (
-    <Fragment>
-      <Card>
-        <CardHeader className='border-bottom'>
-          <CardTitle tag='h4'>Profile Details</CardTitle>
-        </CardHeader>
-        <CardBody className='my-25'>
-          {/* <div className='d-flex'>
+	return (
+		<Fragment>
+			<Card>
+				<CardHeader className="border-bottom">
+					<CardTitle tag="h4">Profile Details</CardTitle>
+				</CardHeader>
+				<CardBody className="my-25">
+					{/* <div className='d-flex'>
             <div className='me-25'>
               <img className='rounded me-50' src={avatar} alt='Generic placeholder image' height='100' width='100' />
             </div>
@@ -85,99 +89,171 @@ const AccountTabs = () => {
               </div>
             </div>
           </div> */}
-          <Form className='pt-50' onSubmit={handleSubmit(onSubmit)}>
-            <Row>
-              {/* USERNAME */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='username'>
-                  Username
-                </Label>
-                <Controller
-                  name='username'
-                  control={control}
-                  render={({ field }) => (
-                    <Input id='username' placeholder='Budi' invalid={errors.firstName && true} {...field} />
-                  )}
-                />
-                {errors && errors.firstName && <FormFeedback>Please enter a valid Username</FormFeedback>}
-              </Col>
-              {/* KELOMPOK */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='phNumber'>
-                  Kelompok
-                </Label>
-                <Cleave
-                  id='kelompok'
-                  name='kelompok'
-                  className='form-control'
-                  placeholder='48'
-                />
-              </Col>
-              {/* HARI */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='hari'>
-                  Hari
-                </Label>
-                <Select
-                  id='hari'
-                  isClearable={false}
-                  className='react-select'
-                  classNamePrefix='select'
-                  options={dayOptions}
-                  theme={selectThemeColors}
-                  defaultValue={dayOptions[0]}
-                />
-              </Col>
-              {/* SHIFT */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='shift'>
-                  Shift
-                </Label>
-                <Select
-                  id='shift'
-                  isClearable={false}
-                  className='react-select'
-                  classNamePrefix='select'
-                  options={shiftOptions}
-                  theme={selectThemeColors}
-                  defaultValue={shiftOptions[0]}
-                />
-              </Col>
-              {/* KELAS */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='kelas'>
-                  Kelas
-                </Label>
-                <Controller
-                  name='kelas'
-                  control={control}
-                  render={({ field }) => (
-                    <Input id='kelas' placeholder='TK-45-02' invalid={errors.firstName && true} {...field} />
-                  )}
-                />
-                {errors && errors.firstName && <FormFeedback>Please enter a valid Class</FormFeedback>}
-              </Col>
-              {/* PHONE NUMBER */}
-              <Col sm='6' className='mb-1'>
-                <Label className='form-label' for='phNumber'>
-                  Number
-                </Label>
-                <Cleave
-                  id='phNumber'
-                  name='phNumber'
-                  className='form-control'
-                  placeholder='1 234 567 8900'
-                  options={{ phone: true, phoneRegionCode: 'ID' }}
-                />
-              </Col>
-            </Row>
-          </Form>
-        </CardBody>
-      </Card>
-      {/* <DeleteAccount /> */}
-      <SecurityTabContent />
-    </Fragment>
-  )
-}
+					<Form className="pt-50" onSubmit={handleSubmit(onSubmit)}>
+						<Row>
+							{/* ID */}
+							<Controller
+								name="id"
+								control={control}
+								render={({ field }) => <Input type="hidden" {...field} />}
+							/>
+							{/* USERNAME */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="username">
+									Username
+								</Label>
+								<Controller
+									name="username"
+									control={control}
+									rules={{ required: "Please fill username!" }}
+									render={({ field }) => (
+										<Input
+											placeholder="Username"
+											invalid={errors.username && true}
+											{...field}
+										/>
+									)}
+								/>
+								{errors && errors.username && (
+									<FormFeedback>{errors.username.message}</FormFeedback>
+								)}
+							</Col>
+							{/* KELOMPOK */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="phNumber">
+									Kelompok
+								</Label>
+								<Controller
+									name="group"
+									control={control}
+									rules={{ required: "Please fill group!" }}
+									render={({ field }) => (
+										<Input
+											placeholder="Group"
+											invalid={errors.group && true}
+											{...field}
+										/>
+									)}
+								/>
+								{errors && errors.group && (
+									<FormFeedback>{errors.group.message}</FormFeedback>
+								)}
+							</Col>
+							{/* HARI */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="hari">
+									Hari
+								</Label>
+								<Controller
+									name="day"
+									control={control}
+									rules={{ required: "Please fill day!" }}
+									render={({ field }) => (
+										<Select
+											theme={selectThemeColors}
+											className="react-select"
+											classNamePrefix="select"
+											options={dayOptions}
+											isClearable
+											{...field}
+										/>
+									)}
+								/>
+								{errors && errors.day && (
+									<FormFeedback>{errors.day.message}</FormFeedback>
+								)}
+							</Col>
+							{/* SHIFT */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="shift">
+									Shift
+								</Label>
+								<Controller
+									name="shift"
+									control={control}
+									rules={{ required: "Please fill shift!" }}
+									render={({ field }) => (
+										<Select
+											theme={selectThemeColors}
+											className="react-select"
+											classNamePrefix="select"
+											options={shiftOptions}
+											isClearable
+											{...field}
+										/>
+									)}
+								/>
+								{errors && errors.shift && (
+									<FormFeedback>{errors.shift.message}</FormFeedback>
+								)}
+							</Col>
+							{/* KELAS */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="kelas">
+									Kelas
+								</Label>
+								<Controller
+									name="classroom"
+									control={control}
+									rules={{ required: "Please fill class!" }}
+									render={({ field }) => (
+										<Input
+											id="kelas"
+											placeholder="TK-45-02"
+											invalid={errors.classroom && true}
+											{...field}
+										/>
+									)}
+								/>
+								{errors && errors.classroom && (
+									<FormFeedback>{errors.classroom.message}</FormFeedback>
+								)}
+							</Col>
+							{/* PHONE NUMBER */}
+							<Col sm="6" className="mb-1">
+								<Label className="form-label" for="phNumber">
+									Number
+								</Label>
+								<Controller
+									name="phoneNumber"
+									control={control}
+									render={({ field }) => (
+										<Input
+											placeholder="08xxxxxx"
+											invalid={errors.firstName && true}
+											{...field}
+										/>
+									)}
+								/>
+							</Col>
 
-export default AccountTabs
+							<Col className="mt-1" sm="12">
+								{/* <Button
+									className="me-1"
+									color="primary"
+									type="submit"
+									disabled={isLoading}
+								>
+									Save changes
+								</Button> */}
+								<Button
+									className="me-1"
+									color="primary"
+									type="submit"
+									disabled={isLoading}
+								>
+									{isLoading && <Spinner color="light" size="sm" />}
+									<span className="ms-50">Save Changes</span>
+								</Button>
+							</Col>
+						</Row>
+					</Form>
+				</CardBody>
+			</Card>
+			{/* <DeleteAccount /> */}
+			<SecurityTabContent />
+		</Fragment>
+	);
+};
+
+export default AccountTabs;
