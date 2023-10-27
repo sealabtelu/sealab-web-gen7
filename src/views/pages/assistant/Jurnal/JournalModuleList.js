b9611998518e7cd6d10ae930a60598f4f45fcf26
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-10-27 18:03:36 +0700

    style: lint fix

--- src/views/pages/assistant/Jurnal/JournalModuleList.js
+++ src/views/pages/assistant/Jurnal/JournalModuleList.js
@@ -1,90 +1,89 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 // ** Custom Components
-import Avatar from "@components/avatar";
+import Avatar from "@components/avatar"
 
 // ** Icons Imports
-import * as Icon from "react-feather";
+import * as Icon from "react-feather"
 
 // ** Reactstrap Imports
 import {
-	Card,
-	CardHeader,
-	CardTitle,
-	CardBody,
-	Button,
-	Input,
-} from "reactstrap";
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Button,
+  Input
+} from "reactstrap"
 
 // ** Store & Actions
-import { useDispatch, useSelector } from "react-redux";
-import { getModules, setJStatus } from "@store/api/module";
+import { useDispatch, useSelector } from "react-redux"
+import { getModules, setJStatus } from "@store/api/module"
 
 // ** Styles
-import "@src/assets/scss/module-list.scss";
+import "@src/assets/scss/module-list.scss"
 
 const PRTModuleList = () => {
-	// ** Store Vars
-	const dispatch = useDispatch();
-	const { modules, isLoading } = useSelector((state) => state.module);
+  // ** Store Vars
+  const dispatch = useDispatch()
+  const { modules, isLoading } = useSelector((state) => state.module)
 
-	useEffect(() => {
-		dispatch(getModules());
-	}, []);
+  useEffect(() => {
+    dispatch(getModules())
+  }, [])
 
-	const renderListModule = () => {
-		if (modules?.length > 0) {
-			return modules.map((item, index) => {
-				return (
-					<div key={item.id} className="module-item">
-						<div className="d-flex">
-							<Avatar
-								className="rounded"
-								color="light-info"
-								content={(index + 1).toString()}
-							/>
-							<div>
-								<h6 className="module-title">
-									Module {index + 1} : {item.name}
-								</h6>
-							</div>
-						</div>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "row",
-								alignItems: "center",
-							}}
-						>
-							<div className="form-switch form-check-success">
-								<Input
-									type="switch"
-									checked={item.isJOpen}
-									disabled={isLoading}
-									onChange={({ target: { checked } }) =>
-										dispatch(setJStatus({ id: item.id, isOpen: checked }))
-									}
-								/>
-							</div>
-						</div>
-					</div>
-				);
-			});
-		} else {
-			return <div>No data available</div>;
-		}
-	};
+  const renderListModule = () => {
+    if (modules?.length > 0) {
+      return modules.map((item, index) => {
+        return (
+          <div key={item.id} className="module-item">
+            <div className="d-flex">
+              <Avatar
+                className="rounded"
+                color="light-info"
+                content={(index + 1).toString()}
+              />
+              <div>
+                <h6 className="module-title">
+                  Module {index + 1} : {item.name}
+                </h6>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center"
+              }}
+            >
+              <div className="form-switch form-check-success">
+                <Input
+                  type="switch"
+                  checked={item.isJOpen}
+                  disabled={isLoading}
+                  onChange={({ target: { checked } }) => dispatch(setJStatus({ id: item.id, isOpen: checked }))
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        )
+      })
+    } else {
+      return <div>No data available</div>
+    }
+  }
 
-	return (
-		<Card className="card-module">
-			<CardHeader>
-				<CardTitle tag="h4">Journal</CardTitle>
-				<Icon.MoreVertical size={18} className="cursor-pointer" />
-			</CardHeader>
-			<CardBody>{renderListModule()}</CardBody>
-		</Card>
-	);
-};
+  return (
+    <Card className="card-module">
+      <CardHeader>
+        <CardTitle tag="h4">Journal</CardTitle>
+        <Icon.MoreVertical size={18} className="cursor-pointer" />
+      </CardHeader>
+      <CardBody>{renderListModule()}</CardBody>
+    </Card>
+  )
+}
 
-export default PRTModuleList;
+export default PRTModuleList
--- src/views/pages/student/HomeAssignment/HAOverlay.js
+++ src/views/pages/student/HomeAssignment/HAOverlay.js
@@ -1,97 +1,96 @@
-import { useState, useEffect } from "react"; // Import React
-import { Card, Button, Row, Col, Container } from "reactstrap";
-import { Link } from "react-router-dom";
-import { selectModule } from "@store/api/module";
-import { useDispatch } from "react-redux";
-import moment from "moment/moment";
+import { useState, useEffect } from "react" // Import React
+import { Card, Button, Row, Col, Container } from "reactstrap"
+import { Link } from "react-router-dom"
+import { selectModule } from "@store/api/module"
+import { useDispatch } from "react-redux"
+import moment from "moment/moment"
 
 const OverlayHA = ({ moduleTitle, moduleNumber, item }) => {
-	const dispatch = useDispatch();
-	const [timeRemaining, setTimeRemaining] = useState(0);
-	const [deadline, setDeadline] = useState("");
-	useEffect(() => {
-		const now = new Date();
-		const midnight = new Date("2023-10-29T12:00:00");
-		setDeadline(midnight);
-		// midnight.setHours(12, 0, 0, 0);
+  const dispatch = useDispatch()
+  const [timeRemaining, setTimeRemaining] = useState(0)
+  const [deadline, setDeadline] = useState("")
+  useEffect(() => {
+    const now = new Date()
+    const midnight = new Date("2023-10-29T12:00:00")
+    setDeadline(midnight)
+    // midnight.setHours(12, 0, 0, 0);
 
-		const timeDiff = midnight - now;
-		if (timeDiff > 0) {
-			setTimeRemaining(Math.floor(timeDiff / 1000));
-		} else {
-			setTimeRemaining(0);
-		}
+    const timeDiff = midnight - now
+    if (timeDiff > 0) {
+      setTimeRemaining(Math.floor(timeDiff / 1000))
+    } else {
+      setTimeRemaining(0)
+    }
 
-		const interval = setInterval(() => {
-			if (timeRemaining > 0) {
-				setTimeRemaining(timeRemaining - 1);
-			} else {
-				clearInterval(interval);
-			}
-		}, 1000);
+    const interval = setInterval(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1)
+      } else {
+        clearInterval(interval)
+      }
+    }, 1000)
 
-		// Membersihkan interval saat komponen tidak lagi digunakan
-		return () => {
-			clearInterval(interval);
-		};
-	}, [timeRemaining]);
+    // Membersihkan interval saat komponen tidak lagi digunakan
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timeRemaining])
 
-	const formatTime = (seconds) => {
-		const hour = Math.floor(seconds / 3600);
-		const minutes = Math.floor((seconds % 3600) / 60);
-		const remainingSeconds = seconds % 60;
-		return `${hour}:${minutes}:${
-			remainingSeconds < 10 ? "0" : ""
-		}${remainingSeconds}`;
-	};
+  const formatTime = (seconds) => {
+    const hour = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const remainingSeconds = seconds % 60
+    return `${hour}:${minutes}:${remainingSeconds < 10 ? "0" : ""
+      }${remainingSeconds}`
+  }
 
-	return (
-		<Card className="card-overlay-jurnal">
-			{/* SUBMIT */}
-			<Container>
-				<Row>
-					<Col>
-						{/* <h2>Tugas Pendahuluan</h2> */}
-						<h3 className="title-overlay">
-							<b>
-								MODUL {moduleNumber}: {moduleTitle}
-							</b>
-						</h3>
-						<p>
-							<b>Due Date: </b>{" "}
-							{moment(deadline).format("ddd DD MMM YYYY h:mm A")}
-						</p>
-						<p>
-							<b>Time Remaining: </b>
-							{formatTime(timeRemaining)}
-						</p>
-						{/* <p>
+  return (
+    <Card className="card-overlay-jurnal">
+      {/* SUBMIT */}
+      <Container>
+        <Row>
+          <Col>
+            {/* <h2>Tugas Pendahuluan</h2> */}
+            <h3 className="title-overlay">
+              <b>
+                MODUL {moduleNumber}: {moduleTitle}
+              </b>
+            </h3>
+            <p>
+              <b>Due Date: </b>{" "}
+              {moment(deadline).format("ddd DD MMM YYYY h:mm A")}
+            </p>
+            <p>
+              <b>Time Remaining: </b>
+              {formatTime(timeRemaining)}
+            </p>
+            {/* <p>
               <b>Time Submitted:</b> &nbsp;{" "}
             </p> */}
-					</Col>
-					<Col xs="12" sm="6">
-						<div className="button-container">
-							<Button
-								tag={Link}
-								to="/student/home-assignment/questionList"
-								color="relief-primary"
-								onClick={() => dispatch(selectModule(item))}
-							>
-								Soal Tugas Pendahuluan
-							</Button>
-							<Button
-								href="https://tinyurl.com/TemplateSoalTP"
-								target="_blank"
-								color="flat-dark"
-							>
-								<b>Template Jawaban TP</b>
-							</Button>
-						</div>
-					</Col>
-				</Row>
-			</Container>
-		</Card>
-	);
-};
+          </Col>
+          <Col xs="12" sm="6">
+            <div className="button-container">
+              <Button
+                tag={Link}
+                to="/student/home-assignment/questionList"
+                color="relief-primary"
+                onClick={() => dispatch(selectModule(item))}
+              >
+                Soal Tugas Pendahuluan
+              </Button>
+              <Button
+                href="https://tinyurl.com/TemplateSoalTP"
+                target="_blank"
+                color="flat-dark"
+              >
+                <b>Template Jawaban TP</b>
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  )
+}
 
-export default OverlayHA;
+export default OverlayHA
