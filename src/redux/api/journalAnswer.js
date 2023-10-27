597ec1f8004cb3723892d0fcf2de0e4cc178e49b
HimejiSoul <perseushilmy@gmail.com>
2023-10-27 20:42:43 +0700

    feat(feedback): update isloading & styling

--- src/redux/api/journalAnswer.js
+++ src/redux/api/journalAnswer.js
@@ -1,62 +1,59 @@
 // ** Axios Imports
-import axios from "axios"
+import axios from "axios";
 
 // ** Redux Imports
-import { createSlice, createAsyncThunk, isAnyOf } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk, isAnyOf } from "@reduxjs/toolkit";
 
-const endpoint = "/journal-answer"
+const endpoint = "/journal-answer";
 
 export const addAnswer = createAsyncThunk(
-  "answer/addAnswer",
-  async (param, { getState }) => {
-    const data = {
-      ...param,
-      idModule: getState().module.selectedModule.id,
-      idStudent: getState().auth.userData.idStudent
-    }
-    return await axios.postForm(endpoint, data)
-  }
-)
+	"answer/addAnswer",
+	async (param, { getState }) => {
+		const data = {
+			...param,
+			idModule: getState().module.selectedModule.id,
+			idStudent: getState().auth.userData.idStudent,
+		};
+		return await axios.postForm(endpoint, data);
+	}
+);
 
 export const getFeedback = createAsyncThunk("journal/getJournal", async () => {
-  return await axios.get("/journal-answer/list").then((res) => {
-    return res.data.data
-  })
-})
+	return await axios.get("/journal-answer/list").then((res) => {
+		return res.data.data;
+	});
+});
 
 export const homeAssignmentAnswerSlice = createSlice({
-  name: "answer",
-  initialState: {
-    feedback: [],
-    isLoading: false
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getFeedback.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(getFeedback.fulfilled, (state, action) => {
-        state.feedback = action.payload
-        state.isLoading = false
-      })
-      .addMatcher(isAnyOf(addAnswer.pending, getFeedback.pending), (state) => {
-        state.isLoading = false
-      })
-      .addMatcher(
-        isAnyOf(
-          addAnswer.fulfilled,
-          addAnswer.rejected,
-          getFeedback.fulfilled,
-          getFeedback.rejected
-        ),
-        (state) => {
-          state.isLoading = false
-        }
-      )
-  }
-})
+	name: "answer",
+	initialState: {
+		feedback: [],
+		isLoading: false,
+	},
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getFeedback.fulfilled, (state, action) => {
+				state.feedback = action.payload;
+				state.isLoading = false;
+			})
+			.addMatcher(isAnyOf(addAnswer.pending, getFeedback.pending), (state) => {
+				state.isLoading = true;
+			})
+			.addMatcher(
+				isAnyOf(
+					addAnswer.fulfilled,
+					addAnswer.rejected,
+					getFeedback.fulfilled,
+					getFeedback.rejected
+				),
+				(state) => {
+					state.isLoading = false;
+				}
+			);
+	},
+});
 
-export const { } = homeAssignmentAnswerSlice.actions
+export const {} = homeAssignmentAnswerSlice.actions;
 
-export default homeAssignmentAnswerSlice.reducer
+export default homeAssignmentAnswerSlice.reducer;
--- src/views/pages/assistant/user/Home.js
+++ src/views/pages/assistant/user/Home.js
@@ -1,110 +1,110 @@
 // ** React Imports
-import { Fragment, useEffect } from "react"
+import { Fragment, useEffect } from "react";
 
 // ** Custom Components
-import Breadcrumbs from "@components/breadcrumbs"
+import Breadcrumbs from "@components/breadcrumbs";
 
 // ** Third Party Components
-import { Card, CardHeader, CardTitle, CardBody, Spinner } from "reactstrap"
+import { Card, CardHeader, CardTitle, CardBody, Spinner } from "reactstrap";
 
 // ** Third Party Components
-import { ChevronDown } from "react-feather"
-import DataTable, { createTheme } from "react-data-table-component"
+import { ChevronDown } from "react-feather";
+import DataTable, { createTheme } from "react-data-table-component";
 
 // ** Styles
-import "@styles/react/libs/tables/react-dataTable-component.scss"
-import { useDispatch, useSelector } from "react-redux"
-import { getFeedback } from "@store/api/journalAnswer"
-import { useSkin } from "@hooks/useSkin"
+import "@styles/react/libs/tables/react-dataTable-component.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { getFeedback } from "@store/api/journalAnswer";
+import { useSkin } from "@hooks/useSkin";
 
 createTheme("dark", {
-  background: {
-    default: "transparent"
-  }
-})
+	background: {
+		default: "transparent",
+	},
+});
 
 const Tables = () => {
-  const dispatch = useDispatch()
-  const { skin } = useSkin()
-  const { feedback, isLoading } = useSelector((state) => state.journalAnswer)
+	const dispatch = useDispatch();
+	const { skin } = useSkin();
+	const { feedback, isLoading } = useSelector((state) => state.journalAnswer);
 
-  useEffect(() => {
-    dispatch(getFeedback())
-    console.log(feedback)
-  }, [])
+	useEffect(() => {
+		dispatch(getFeedback());
+		console.log(feedback);
+	}, []);
 
-  const basicColumns = [
-    {
-      name: "Name",
-      // sortable: true,
-      minWidth: "225px",
-      selector: (row) => row.studentInfo.name
-    },
-    {
-      name: "Day",
-      sortable: true,
-      selector: (row) => {
-        const days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-        return days[row.studentInfo.day - 1]
-      }
-    },
-    {
-      name: "Shift",
-      // sortable: true,
-      selector: (row) => `Shift ${row.studentInfo.shift}`
-    },
-    {
-      name: "Feedback untuk asisten",
-      // sortable: true,
-      selector: (row) => row.assistantFeedback
-    },
-    {
-      name: "Feedback untuk praktikum",
-      // sortable: true,
-      // minWidth: "150px",
-      selector: (row) => row.sessionFeedback
-    },
-    {
-      name: "Feedback untuk Laboratorium",
-      // sortable: true,
-      minWidth: "150px",
-      selector: (row) => row.laboratoryFeedback
-    }
-  ]
+	const basicColumns = [
+		{
+			name: "Name",
+			// sortable: true,
+			minWidth: "225px",
+			selector: (row) => row.studentInfo.name,
+		},
+		{
+			name: "Day",
+			sortable: true,
+			selector: (row) => {
+				const days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+				return days[row.studentInfo.day - 1];
+			},
+		},
+		{
+			name: "Shift",
+			selector: (row) => `Shift ${row.studentInfo.shift}`,
+		},
+		{
+			name: "Feedback untuk asisten",
+			wrap: true,
+			width: "400px",
+			selector: (row) => row.assistantFeedback,
+		},
+		{
+			name: "Feedback untuk praktikum",
+			wrap: true,
+			width: "400px",
+			selector: (row) => row.sessionFeedback,
+		},
+		{
+			name: "Feedback untuk Laboratorium",
+			width: "400px",
+			// wrap: true,
+			selector: (row) => row.laboratoryFeedback,
+		},
+	];
 
-  return (
-    <Fragment>
-      <Breadcrumbs
-        title="Student"
-        data={[{ title: "Datatables" }, { title: "Datatables Basic" }]}
-      />
-      <Card className="overflow-hidden">
-        <CardHeader>
-          <CardTitle tag="h4">Feedback List</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <div className="react-dataTable">
-            <DataTable
-              noHeader
-              pagination
-              data={feedback}
-              columns={basicColumns}
-              progressPending={isLoading}
-              theme={skin}
-              className="react-dataTable"
-              sortIcon={<ChevronDown size={10} />}
-              paginationRowsPerPageOptions={[10, 25, 50, 100]}
-              progressComponent={
-                <div className="d-flex justify-content-center my-1">
-                  <Spinner color="primary" />
-                </div>
-              }
-            />
-          </div>
-        </CardBody>
-      </Card>
-    </Fragment>
-  )
-}
+	return (
+		<Fragment>
+			<Breadcrumbs
+				title="Student"
+				data={[{ title: "Datatables" }, { title: "Datatables Basic" }]}
+			/>
+			<Card className="overflow-hidden">
+				<CardHeader>
+					<CardTitle tag="h4">Feedback List</CardTitle>
+				</CardHeader>
+				<CardBody>
+					<div className="react-dataTable">
+						<DataTable
+							noHeader
+							pagination
+							data={feedback}
+							columns={basicColumns}
+							progressPending={isLoading}
+							theme={skin}
+							className="react-dataTable"
+							sortIcon={<ChevronDown size={10} />}
+							paginationRowsPerPageOptions={[10, 25, 50, 100]}
+							progressComponent={
+								<div className="d-flex justify-content-center my-1">
+									<Spinner color="primary" />
+								</div>
+							}
+						/>
+					</div>
+				</CardBody>
+			</Card>
+		</Fragment>
+	);
+};
 
-export default Tables
+export default Tables;
