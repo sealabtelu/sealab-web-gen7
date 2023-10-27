90c9d087f1911efb0792ad13ef86e60457ae16ed
HimejiSoul <perseushilmy@gmail.com>
2023-10-27 16:42:05 +0700

    feat(route): add jurnal route

--- src/redux/api/module.js
+++ src/redux/api/module.js
@@ -1,126 +1,177 @@
 // ** Axios Imports
-import axios from 'axios'
+import axios from "axios";
 
 // ** Redux Imports
-import { createSlice, createAsyncThunk, isAnyOf } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, isAnyOf } from "@reduxjs/toolkit";
 
-const endpoint = "/module"
+const endpoint = "/module";
 
-export const getModules = createAsyncThunk('module/getModules', async () => {
-  return await axios.get(`${endpoint}/list`).then(res => {
-    return res.data.data
-  })
-})
+export const getModules = createAsyncThunk("module/getModules", async () => {
+	return await axios.get(`${endpoint}/list`).then((res) => {
+		return res.data.data;
+	});
+});
 
-export const getAllSubmissions = createAsyncThunk('question/getAllSubmissions', async (seelabsId, { getState }) => {
-  const data = {
-    seelabsId,
-    group: getState().seelabs.currentDSG.group
-  }
-  return await axios.post(`${endpoint}/submission/list`, data).then(res => {
-    return res.data.data
-  })
-})
+export const getAllSubmissions = createAsyncThunk(
+	"question/getAllSubmissions",
+	async (seelabsId, { getState }) => {
+		const data = {
+			seelabsId,
+			group: getState().seelabs.currentDSG.group,
+		};
+		return await axios.post(`${endpoint}/submission/list`, data).then((res) => {
+			return res.data.data;
+		});
+	}
+);
 
-export const getPASubmissions = createAsyncThunk('question/getPASubmissions', async (_, { getState }) => {
-  return await axios.get(`${endpoint}/submission/pa/${getState().auth.userData.idStudent}`).then(res => {
-    return res.data.data
-  })
-})
+export const getPASubmissions = createAsyncThunk(
+	"question/getPASubmissions",
+	async (_, { getState }) => {
+		return await axios
+			.get(`${endpoint}/submission/pa/${getState().auth.userData.idStudent}`)
+			.then((res) => {
+				return res.data.data;
+			});
+	}
+);
 
-export const getPRTSubmissions = createAsyncThunk('question/getPRTSubmissions', async (_, { getState }) => {
-  return await axios.get(`${endpoint}/submission/prt/${getState().auth.userData.idStudent}`).then(res => {
-    return res.data.data
-  })
-})
+export const getPRTSubmissions = createAsyncThunk(
+	"question/getPRTSubmissions",
+	async (_, { getState }) => {
+		return await axios
+			.get(`${endpoint}/submission/prt/${getState().auth.userData.idStudent}`)
+			.then((res) => {
+				return res.data.data;
+			});
+	}
+);
 
-export const getJSubmissions = createAsyncThunk('question/getJSubmissions', async (_, { getState }) => {
-  return await axios.get(`${endpoint}/submission/j/${getState().auth.userData.idStudent}`).then(res => {
-    return res.data.data
-  })
-})
+export const getJSubmissions = createAsyncThunk(
+	"question/getJSubmissions",
+	async (_, { getState }) => {
+		return await axios
+			.get(`${endpoint}/submission/j/${getState().auth.userData.idStudent}`)
+			.then((res) => {
+				return res.data.data;
+			});
+	}
+);
 
-export const setPAStatus = createAsyncThunk('module/setPAStatus', async (param) => {
-  return await axios.post(`${endpoint}/set-pa-status`, param).then(res => {
-    return res.data.data
-  })
-})
+export const setPAStatus = createAsyncThunk(
+	"module/setPAStatus",
+	async (param) => {
+		return await axios.post(`${endpoint}/set-pa-status`, param).then((res) => {
+			return res.data.data;
+		});
+	}
+);
 
-export const setPRTStatus = createAsyncThunk('module/setPRTStatus', async (param) => {
-  return await axios.post(`${endpoint}/set-prt-status`, param).then(res => {
-    return res.data.data
-  })
-})
+export const setPRTStatus = createAsyncThunk(
+	"module/setPRTStatus",
+	async (param) => {
+		return await axios.post(`${endpoint}/set-prt-status`, param).then((res) => {
+			return res.data.data;
+		});
+	}
+);
+
+export const setJStatus = createAsyncThunk(
+	"module/setJStatus",
+	async (param) => {
+		return await axios.post(`${endpoint}/set-j-status`, param).then((res) => {
+			return res.data.data;
+		});
+	}
+);
 
 const initialSelectedModule = () => {
-  const item = window.localStorage.getItem('selectedModule')
-  return item ? JSON.parse(item) : {}
-}
+	const item = window.localStorage.getItem("selectedModule");
+	return item ? JSON.parse(item) : {};
+};
 
 export const moduleSlice = createSlice({
-  name: 'module',
-  initialState: {
-    modules: [],
-    submissions: [],
-    isLoading: false,
-    selectedModule: initialSelectedModule()
-  },
-  reducers: {
-    selectModule: (state, action) => {
-      state.selectedModule = action.payload
-      localStorage.setItem('selectedModule', JSON.stringify(action.payload))
-    },
-    clearSubmissions: (state) => {
-      state.submissions = []
-    }
-  },
-  extraReducers: builder => {
-    builder
-      .addCase(getAllSubmissions.fulfilled, (state, action) => {
-        const baseURL = `${import.meta.env.VITE_API_BASE_URL}upload/`
-        state.submissions = action.payload.map(item => (
-          {
-            ...item,
-            paFilePath: item.paFilePath && `${baseURL}${item.paFilePath}`,
-            jFilePath: item.jFilePath && `${baseURL}${item.jFilePath}`
-          }
-        ))
-      })
-      .addMatcher(isAnyOf(
-        getModules.fulfilled,
-        getPASubmissions.fulfilled,
-        getPRTSubmissions.fulfilled,
-        getJSubmissions.fulfilled,
-        setPAStatus.fulfilled,
-        setPRTStatus.fulfilled
-      ), (state, action) => {
-        state.modules = action.payload
-      })
-      .addMatcher(isAnyOf(
-        getModules.pending,
-        getPASubmissions.pending,
-        getPRTSubmissions.pending,
-        getJSubmissions.pending,
-        getAllSubmissions.pending,
-        setPAStatus.pending,
-        setPRTStatus.pending
-      ), (state) => {
-        state.isLoading = true
-      })
-      .addMatcher(isAnyOf(
-        getModules.fulfilled, getModules.rejected,
-        getPASubmissions.fulfilled, getPASubmissions.rejected,
-        getPRTSubmissions.fulfilled, getPRTSubmissions.rejected,
-        getJSubmissions.fulfilled, getJSubmissions.rejected,
-        getAllSubmissions.fulfilled, getAllSubmissions.rejected,
-        setPAStatus.fulfilled, setPAStatus.rejected,
-        setPRTStatus.fulfilled, setPRTStatus.rejected
-      ), (state) => {
-        state.isLoading = false
-      })
-  }
-})
+	name: "module",
+	initialState: {
+		modules: [],
+		submissions: [],
+		isLoading: false,
+		selectedModule: initialSelectedModule(),
+	},
+	reducers: {
+		selectModule: (state, action) => {
+			state.selectedModule = action.payload;
+			localStorage.setItem("selectedModule", JSON.stringify(action.payload));
+		},
+		clearSubmissions: (state) => {
+			state.submissions = [];
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getAllSubmissions.fulfilled, (state, action) => {
+				const baseURL = `${import.meta.env.VITE_API_BASE_URL}upload/`;
+				state.submissions = action.payload.map((item) => ({
+					...item,
+					paFilePath: item.paFilePath && `${baseURL}${item.paFilePath}`,
+					jFilePath: item.jFilePath && `${baseURL}${item.jFilePath}`,
+				}));
+			})
+			.addMatcher(
+				isAnyOf(
+					getModules.fulfilled,
+					getPASubmissions.fulfilled,
+					getPRTSubmissions.fulfilled,
+					getJSubmissions.fulfilled,
+					setPAStatus.fulfilled,
+					setPRTStatus.fulfilled,
+					setJStatus.fulfilled
+				),
+				(state, action) => {
+					state.modules = action.payload;
+				}
+			)
+			.addMatcher(
+				isAnyOf(
+					getModules.pending,
+					getPASubmissions.pending,
+					getPRTSubmissions.pending,
+					getJSubmissions.pending,
+					getAllSubmissions.pending,
+					setPAStatus.pending,
+					setPRTStatus.pending,
+					setJStatus.pending
+				),
+				(state) => {
+					state.isLoading = true;
+				}
+			)
+			.addMatcher(
+				isAnyOf(
+					getModules.fulfilled,
+					getModules.rejected,
+					getPASubmissions.fulfilled,
+					getPASubmissions.rejected,
+					getPRTSubmissions.fulfilled,
+					getPRTSubmissions.rejected,
+					getJSubmissions.fulfilled,
+					getJSubmissions.rejected,
+					getAllSubmissions.fulfilled,
+					getAllSubmissions.rejected,
+					setPAStatus.fulfilled,
+					setPAStatus.rejected,
+					setPRTStatus.fulfilled,
+					setPRTStatus.rejected,
+					setJStatus.fulfilled,
+					setJStatus.rejected
+				),
+				(state) => {
+					state.isLoading = false;
+				}
+			);
+	},
+});
 
-export const { selectModule, clearSubmissions } = moduleSlice.actions
+export const { selectModule, clearSubmissions } = moduleSlice.actions;
 
-export default moduleSlice.reducer
+export default moduleSlice.reducer;
--- src/router/routes/Assistant.js
+++ src/router/routes/Assistant.js
@@ -1,124 +1,172 @@
-import { lazy } from 'react'
+import { lazy } from "react";
 
-import { Mail, Home, Clipboard, Bookmark, Edit3, PenTool, Book } from "react-feather"
+import {
+	Mail,
+	Home,
+	Clipboard,
+	Bookmark,
+	Edit3,
+	PenTool,
+	Book,
+	AlertTriangle,
+} from "react-feather";
 
-const HAModuleList = lazy(() => import("../../views/pages/assistant/HomeAssignment/HAModuleList"))
-const HAQuestionList = lazy(() => import("../../views/pages/assistant/HomeAssignment/HAQuestionList"))
-const HAQuestion = lazy(() => import("../../views/pages/assistant/HomeAssignment/HAQuestion"))
-const PRTModuleList = lazy(() => import("../../views/pages/assistant/PreTest/PRTModuleList"))
-const PRTQuestionList = lazy(() => import("../../views/pages/assistant/PreTest/PRTQuestionList.js"))
-const PRTQuestion = lazy(() => import("../../views/pages/assistant/PreTest/PRTQuestion"))
-const SelectGroup = lazy(() => import("../../views/pages/assistant/InputScore/SelectGroup"))
-const InputScore = lazy(() => import("../../views/pages/assistant/InputScore/InputScore"))
-const BAP = lazy(() => import("../../views/pages/assistant/BAP/BAP"))
-const HasilInput = lazy(() => import("../../views/pages/assistant/HasilInput/HasilInput"))
-const ViewNilai = lazy(() => import("../../views/pages/assistant/HasilInput/ViewNilai"))
-const UpdateScore = lazy(() => import("../../views/pages/assistant/HasilInput/UpdateScore"))
-const StudentList = lazy(() => import("../../views/pages/assistant/user/StudentList"))
+const HAModuleList = lazy(() =>
+	import("../../views/pages/assistant/HomeAssignment/HAModuleList")
+);
+const HAQuestionList = lazy(() =>
+	import("../../views/pages/assistant/HomeAssignment/HAQuestionList")
+);
+const HAQuestion = lazy(() =>
+	import("../../views/pages/assistant/HomeAssignment/HAQuestion")
+);
+const PRTModuleList = lazy(() =>
+	import("../../views/pages/assistant/PreTest/PRTModuleList")
+);
+const PRTQuestionList = lazy(() =>
+	import("../../views/pages/assistant/PreTest/PRTQuestionList.js")
+);
+const PRTQuestion = lazy(() =>
+	import("../../views/pages/assistant/PreTest/PRTQuestion")
+);
+const Journal = lazy(() =>
+	import("../../views/pages/assistant/Jurnal/JournalModuleList")
+);
+const SelectGroup = lazy(() =>
+	import("../../views/pages/assistant/InputScore/SelectGroup")
+);
+const InputScore = lazy(() =>
+	import("../../views/pages/assistant/InputScore/InputScore")
+);
+const BAP = lazy(() => import("../../views/pages/assistant/BAP/BAP"));
+const HasilInput = lazy(() =>
+	import("../../views/pages/assistant/HasilInput/HasilInput")
+);
+const ViewNilai = lazy(() =>
+	import("../../views/pages/assistant/HasilInput/ViewNilai")
+);
+const UpdateScore = lazy(() =>
+	import("../../views/pages/assistant/HasilInput/UpdateScore")
+);
+const StudentList = lazy(() =>
+	import("../../views/pages/assistant/user/StudentList")
+);
 
 const AssistantRoutes = [
-    {
-        path: "/assistant/home",
-        element: <StudentList />
-    },
-    {
-        path: "/assistant/preliminary-assignment",
-        element: <HAModuleList />
-    },
-    {
-        path: "/assistant/preliminary-assignment/question-list",
-        element: <HAQuestionList />
-    },
-    {
-        path: "/assistant/preliminary-assignment/question/:action",
-        element: <HAQuestion />
-    },
-    {
-        path: "/assistant/pre-test",
-        element: <PRTModuleList />
-    },
-    {
-        path: "/assistant/pre-test/question-list",
-        element: <PRTQuestionList />
-    },
-    {
-        path: "/assistant/pre-test/question/:action",
-        element: <PRTQuestion />
-    },
-    {
-        path: "/assistant/select-group",
-        element: <SelectGroup />
-    },
-    {
-        path: "/assistant/select-group/input-score",
-        element: <InputScore />
-    },
-    {
-        path: "/assistant/BAP",
-        element: <BAP />
-    },
-    {
-        path: "/assistant/hasil-input",
-        element: <HasilInput />
-    },
-    {
-        path: "/assistant/hasil-input/view-nilai",
-        element: <ViewNilai />
-    },
-    {
-        path: "/assistant/hasil-input/update-score",
-        element: <UpdateScore />
-    }
-]
+	{
+		path: "/assistant/home",
+		element: <StudentList />,
+	},
+	{
+		path: "/assistant/preliminary-assignment",
+		element: <HAModuleList />,
+	},
+	{
+		path: "/assistant/preliminary-assignment/question-list",
+		element: <HAQuestionList />,
+	},
+	{
+		path: "/assistant/preliminary-assignment/question/:action",
+		element: <HAQuestion />,
+	},
+	{
+		path: "/assistant/pre-test",
+		element: <PRTModuleList />,
+	},
+	{
+		path: "/assistant/pre-test/question-list",
+		element: <PRTQuestionList />,
+	},
+	{
+		path: "/assistant/pre-test/question/:action",
+		element: <PRTQuestion />,
+	},
+	{
+		path: "/assistant/journal",
+		element: <Journal />,
+	},
+	{
+		path: "/assistant/select-group",
+		element: <SelectGroup />,
+	},
+	{
+		path: "/assistant/select-group/input-score",
+		element: <InputScore />,
+	},
+	{
+		path: "/assistant/BAP",
+		element: <BAP />,
+	},
+	{
+		path: "/assistant/hasil-input",
+		element: <HasilInput />,
+	},
+	{
+		path: "/assistant/hasil-input/view-nilai",
+		element: <ViewNilai />,
+	},
+	{
+		path: "/assistant/hasil-input/update-score",
+		element: <UpdateScore />,
+	},
+];
 
 export const AssistantMenu = [
-    {
-        id: "home",
-        title: "Home",
-        icon: <Home size={20} />,
-        navLink: "/assistant/home"
-    },
-    {
-        id: "homeAssignment",
-        title: "Home Assignment",
-        icon: <Clipboard size={20} />,
-        navLink: "/assistant/preliminary-assignment"
-    },
-    {
-        id: "preTest",
-        title: "Pre Test",
-        icon: <Edit3 size={20} />,
-        navLink: "/assistant/pre-test"
-    },
-    // {
-    //     id: "journal",
-    //     title: "Journal",
-    //     icon: <PenTool size={20} />,
-    //     // navLink: "/assistant/student"
-    // },
-    {
-        id: "inputScore",
-        title: "Input Score",
-        icon: <Clipboard size={20} />,
-        navLink: "/assistant/select-group"
-    },
-    {
-        id: "bap",
-        title: "BAP",
-        icon: <Bookmark size={20} />,
-        navLink: "/assistant/BAP"
-    },
-    {
-        id: "hasilInput",
-        title: "Hasil Input",
-        icon: <Bookmark size={20} />,
-        navLink: "/assistant/hasil-input"
-    }
-]
+	{
+		id: "home",
+		title: "Home",
+		icon: <Home size={20} />,
+		navLink: "/assistant/home",
+	},
+	{
+		id: "homeAssignment",
+		title: "Home Assignment",
+		icon: <Clipboard size={20} />,
+		navLink: "/assistant/preliminary-assignment",
+	},
+	{
+		id: "preTest",
+		title: "Pre Test",
+		icon: <Edit3 size={20} />,
+		navLink: "/assistant/pre-test",
+	},
+	{
+		id: "journal",
+		title: "Journal",
+		icon: <PenTool size={20} />,
+		navLink: "/assistant/journal",
+	},
+	{
+		id: "inputScore",
+		title: "Input Score",
+		icon: <Clipboard size={20} />,
+		navLink: "/assistant/select-group",
+	},
+	{
+		id: "bap",
+		title: "BAP",
+		icon: <Bookmark size={20} />,
+		navLink: "/assistant/BAP",
+	},
+	{
+		id: "hasilInput",
+		title: "Hasil Input",
+		icon: <Bookmark size={20} />,
+		navLink: "/assistant/hasil-input",
+	},
+	{
+		id: "sanksiBermasalah",
+		title: "Sanksi Bermasalah",
+		icon: <AlertTriangle size={20} />,
+		navLink: "https://bit.ly/CatatanPraktikanBermasalah",
+		externalLink: true,
+		newTab: true,
+	},
+];
 
-export default AssistantRoutes.map(item => ({
-    ...item,
-    meta: {
-        role: 'Assistant'
-    }
-}))
+export default AssistantRoutes.map((item) => ({
+	...item,
+	meta: {
+		role: "Assistant",
+	},
+}));
