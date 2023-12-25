3cbc2c8ac7d16c76a82b62944c43d911e6f1eebf
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-12-25 19:04:57 +0700

    fix: rollup resolver esm subpath imports

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
 
@@ -9,7 +10,7 @@ export default () => {
   return defineConfig({
     plugins: [react()],
     define: {
-      global: 'globalThis'
+      global: {} //'globalThis'
     },
     server: {
       port: 3000,
@@ -88,8 +89,10 @@ export default () => {
     },
     build: {
       rollupOptions: {
-        external: ['#swagger-ui'],
-        plugins: [rollupNodePolyFill()]
+        plugins: [
+          rollupNodePolyFill(),
+          nodeResolve({ browser: true })
+        ]
       }
     }
   })
