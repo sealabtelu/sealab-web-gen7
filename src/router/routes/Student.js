f3be07f53270215fe3b8f3d6db4bf3c229596dd1
Axel David <axeldavid1521@gmail.com>
2024-02-21 17:47:11 +0700

    style: hide pretest page

--- src/router/routes/Student.js
+++ src/router/routes/Student.js
@@ -54,12 +54,12 @@ export const StudentMenu = [
     icon: <BookOpen size={20} />,
     navLink: "/student/home-assignment"
   },
-  {
-    id: "preTest",
-    title: "Pre Test",
-    icon: <Edit3 size={20} />,
-    navLink: "student/pre-test"
-  },
+  // {
+  //   id: "preTest",
+  //   title: "Pre Test",
+  //   icon: <Edit3 size={20} />,
+  //   navLink: "student/pre-test"
+  // },
   {
     id: "journal",
     title: "Journal",
